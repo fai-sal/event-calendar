b88c5aa0f4015495e002209dc968164721a1d1aa
Faisal <faisalcse29@gmail.com>
2018-10-25 18:03:35 +0600

    generic button refactored

--- src/components/GenericButton.js
+++ src/components/GenericButton.js
@@ -1,19 +1,10 @@
 import React from 'react';
 import '../styles/genericButton.css'
 export default ({ children, customStyle, isDisabled, ...restProps }) => {
-    return <button disabled={isDisabled} onClick={restProps.actionHandler} style={{ ...styles.buttonStyle, ...customStyle }}{...restProps}>{children}</button>
-}
-const styles ={
-    buttonStyle:  {
-        borderColor: '#F6D449',
-        borderRadius: '12px',
-        paddingLeft: '15%',
-        paddingRight: '15%',
-        fontWeight: '300',
-        paddingTop: '.1rem',
-        paddingBottom: '.1rem',
-        fontSize: '105%',
-        fontFamily: "'Source Sans Pro', sans-serif",
-        outline:'0px'
-    }
+    return <button
+        className="genericButton"
+        onClick={restProps.actionHandler}
+    >
+        {children}
+    </button>
 }
