386510adb675dde68e9c863745de0a06703f5a39
Faisal <faisalcse29@gmail.com>
2018-10-25 10:19:03 +0600

    renamed reducer file name to index.js from rootreducer.js under reducers folder

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Home from './components/Home';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux'
-import rootReducer from './reducers/rootReducer';
+import rootReducer from './reducers';
 import thunk from 'redux-thunk'
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
