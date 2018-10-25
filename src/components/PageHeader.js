e3f98c9f9bdc01b02eb61cfd8aa94a195d5ceeb2
Faisal <faisalcse29@gmail.com>
2018-10-25 15:17:38 +0600

    pageHeader component refactored

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -1,26 +1,16 @@
 import React from 'react'
+import '../styles/pageHeader.css'
 import MonthTitle from './MonthTitle'
 import ColorPicker from './ColorPicker'
 import { Container, Row } from 'reactstrap'
 const PageHeader = () => {
     return (
-        <Container fluid style={{ width: '100%' }}>
-            <Row style={styles.headerContainer}>
+        <Container fluid className="headerContainer" >
+            <Row className="header">
                 <MonthTitle />
                 <ColorPicker />
             </Row>
         </Container>
-
     )
 }
 export default PageHeader
-const styles = {
-    headerContainer: {
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'center',
-        margin: '0px',
-        marginTop: '2%',
-        marginBottom: '1%',
-    }
-}
